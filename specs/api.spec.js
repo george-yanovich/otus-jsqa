fc77744a95242aae2edea2e9e5174906e6473c31
George Yanovich <georgy@Georges-MacBook-Pro-2.local>
2023-02-06 09:26:35 +0300

    attempt at homework

---
+++ specs/api.spec.js
@@ -0,0 +1,122 @@
+"use strict";
+
+const axios = require('axios');
+
+const goodUser = {
+    "userName": "duuGGGi096",
+    "password": "Azhn9!ess"
+};
+
+const badUser = {
+    "userName": "xyz",
+    "password": "xyz!ess"
+};
+
+describe('API тесты на сервис bookstore', () => {
+
+    describe('Успешное создание пользователя и генерация токена авторизации.', () => {
+
+
+        it('Создание пользователя успешно', async () => {
+            const config = {
+                method: 'post',
+                url: 'https://bookstore.demoqa.com/Account/v1/User',
+                data: goodUser,
+                headers: {
+                    'accept': 'application/json; charset=utf-8',
+                    'Content-Type': 'application/json',
+                    'Connection': 'keep-alive'
+                },
+            };
+    
+            const resp = await axios(config);
+            let uiid = resp.data.userID;
+            console.log(uiid);
+            expect(resp.status).toEqual(201);
+        });
+
+        it('Генерация токена успешно', async () => {
+            const config = {
+                method: 'post',
+                url: 'https://bookstore.demoqa.com/Account/v1/GenerateToken',
+                data: goodUser,
+                headers: {
+                    'accept': 'application/json; charset=utf-8',
+                    'Content-Type': 'application/json',
+                    'Connection': 'keep-alive'
+                },
+            };
+            const resp = await axios(config);
+            let tkn = resp.data.token;
+            expect(resp.status).toEqual(200);
+            expect(resp.data.status).toEqual('Success')
+            expect(resp.data.result).toEqual('User authorized successfully.')
+            console.log(tkn);
+
+    });
+});
+
+describe('Ошибки при создании пользователя и генерации токена авторизации', () => {
+
+    it('Создание пользователя с ошибкой, логин уже используется', async () => {
+        const config = {
+            method: 'post',
+            url: 'https://bookstore.demoqa.com/Account/v1/User',
+            data: goodUser,
+            headers: {
+                'accept': 'application/json; charset=utf-8',
+                'Content-Type': 'application/json',
+                'Connection': 'keep-alive'
+            },
+        };
+        try {
+            const resp = await axios(config);
+        } catch (e) {
+            expect(e.response.status).toEqual(406);
+        }
+    });
+
+    it('Создание пользователя c ошибкой, пароль не подходит', async () => {
+        const config = {
+            method: 'post',
+            url: 'https://bookstore.demoqa.com/Account/v1/User',
+            data: badUser,
+            headers: {
+                'accept': 'application/json; charset=utf-8',
+                'Content-Type': 'application/json',
+                'Connection': 'keep-alive'
+            },
+        };
+        try {
+            const resp = await axios(config);
+        } catch (e) {
+            expect(e.response.data.code).toEqual('1300');
+        }
+    });
+
+    it('Генерация токена c ошибкой', async () => {
+        const config = {
+            method: 'post',
+            url: 'https://bookstore.demoqa.com/Account/v1/GenerateToken',
+            data: badUser,
+            headers: {
+                'accept': 'application/json; charset=utf-8',
+                'Content-Type': 'application/json',
+                'Connection': 'keep-alive'
+            },
+        };
+        const resp = await axios(config);
+        expect(resp.status).toEqual(200);
+        expect(resp.data.status).toEqual('Failed')
+        expect(resp.data.result).toEqual('User authorization failed.')
+
+    });
+
+});
+
+
+
+
+
+});
+
--- src/tale.js
+++ src/tale.js
@@ -1,32 +1,39 @@
 
 
 
-function kolobok(characterName) {
- switch (characterName) {
-    case 'дедушка':
-        console.log('Я от дедушки ушел!');
-        break;
-    case 'заяц':
-        console.log('Я от зайца ушел!');
-        break;
-    case 'лиса':
-        console.log('Меня съели!');
-        break;
- }
-}
+// function kolobok(characterName) {
+//  switch (characterName) {
+//     case 'дедушка':
+//         console.log('Я от дедушки ушел!');
+//         break;
+//     case 'заяц':
+//         console.log('Я от зайца ушел!');
+//         break;
+//     case 'лиса':
+//         console.log('Меня съели!');
+//         break;
+//  }
+// }
 
 
-function newYear(characterName) {
-    switch (characterName) {
-        case 'Дед Мороз':
-            console.log(`${characterName}! ${characterName}! ${characterName}!`);
-            break;
-        case 'Снегурочка':
-            console.log(`${characterName}! ${characterName}! ${characterName}!`)
-            break;
-    }
-}
+// function newYear(characterName) {
+//     switch (characterName) {
+//         case 'Дед Мороз':
+//             console.log(`${characterName}! ${characterName}! ${characterName}!`);
+//             break;
+//         case 'Снегурочка':
+//             console.log(`${characterName}! ${characterName}! ${characterName}!`)
+//             break;
+//     }
+// }
 
+
+import { faker } from '@faker-js/faker';
+
+const randomName = faker.internet.userName(); // Rowan Nikolaus
+const randomEmail = faker.internet.password(); // Kassandra.Haley@erich.biz
+
+console.log(randomName, randomEmail);
 
 
 
